b33e3d86675be473b39565f85f2b50f7c4ae77b2
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2026-02-10 15:18:58 +0530

    Add create new livestream feature and update dependencies
    - Implemented a new route and view for creating a new livestream.
    - Updated package.json and package-lock.json to include 'ffmpeg' and 'sarvamai' dependencies.
    - Added link to create new livestream in the home page.

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ import express from 'express';
 import ejs from 'ejs';
 import dotenv from 'dotenv';
 import session from 'express-session';
+import { spawn } from "child_process";
+import { SarvamAIClient } from 'sarvamai';
+
 
 dotenv.config();
 
 const app = express();
 
+const client = new SarvamAIClient({
+    apiSubscriptionKey: process.env.SARVAM_API_KEY
+});
+
 app.get("/",async (req,res)=>{
     res.render("home.ejs");
+})
+
+app.get("/create-new-livestream",async(req,res)=>{
+  res.render("new-live-stream.ejs");
 })
 
 app.get("/dashboard",async (req,res)=>{
     res.render("dashboard.ejs");
 })
+
+
 app.listen(process.env.PORT,()=>{
     console.log("Running on http://localhost:3000");
 })
