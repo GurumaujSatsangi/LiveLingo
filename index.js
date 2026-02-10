81a28e5ac0cf9f1aefbfe41b7f58db8b2bc34644
Gurumauj Satsangi <gurumaujsatsangi@gmail.com>
2026-02-10 15:44:56 +0530

    Enhance livestream creation form with labels and improve styling; add ffmpeg-static dependency for video processing

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import ejs from 'ejs';
 import dotenv from 'dotenv';
 import session from 'express-session';
 import { spawn } from "child_process";
+import ffmpegPath from "ffmpeg-static";
 import { SarvamAIClient } from 'sarvamai';
 
 
@@ -25,6 +26,32 @@ app.get("/create-new-livestream",async(req,res)=>{
 app.get("/dashboard",async (req,res)=>{
     res.render("dashboard.ejs");
 })
+
+
+const ffmpeg = spawn(ffmpegPath, [
+  "-reconnect", "1",
+  "-reconnect_streamed", "1",
+  "-reconnect_delay_max", "2",
+  "-i", process.env.CLOUDINARY_LIVESTREAM_HLS_URL,
+  "-vn",
+  "-ar", "16000",
+  "-ac", "1",
+  "-f", "s16le",
+  "pipe:1"
+]);
+
+ffmpeg.stdout.on("data", chunk => {
+  console.log(chunk);
+});
+
+ffmpeg.stderr.on("data", err => {
+  console.error("FFmpeg:", err.toString());
+});
+
+ffmpeg.on("close", code => {
+  console.log("FFmpeg exited with code", code);
+});
+
 
 
 app.listen(process.env.PORT,()=>{
